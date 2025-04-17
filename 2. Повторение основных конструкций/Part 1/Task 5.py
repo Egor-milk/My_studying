e93d0b74484fbd57550c57efc6efb48a94b267d6
Egor-milk <mrpixelegor@gmail.com>
2025-04-17 07:58:06 +0700

    commit

--- 2. Повторение основных конструкций/Part 1/Task 5.py
+++ 2. Повторение основных конструкций/Part 1/Task 5.py
@@ -1,1 +1,23 @@
-from autotest import test
+from autotest import take_all_files, take_input_output_values
+
+
+def test(input_values, output_values):
+    for inp, out in zip(input_values, output_values):
+        exec(inp)
+        print(*out)
+        print()
+
+
+def convert(our_string):
+    lst = list(filter(lambda x: x.isalpha(), our_string))
+    count_lower = sum(map(lambda x: x == x.lower(), lst))
+    count_upper = sum(map(lambda x: x == x.upper(), lst))
+    if count_upper > count_lower:
+        our_string = our_string.upper()
+    else:
+        our_string = our_string.lower()
+    return our_string
+
+input_values, output_values = take_input_output_values(take_all_files())
+#вывод содержимого файлов
+test(input_values, output_values)
