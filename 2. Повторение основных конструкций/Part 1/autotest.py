dad76b1f44bbb81cfe616de31b96102dca19cf2f
Egor <mrpixelegor@mail.ru>
2025-04-17 07:58:06 +0700

    commit

--- 2. Повторение основных конструкций/Part 1/autotest.py
+++ 2. Повторение основных конструкций/Part 1/autotest.py
@@ -1,2 +1,21 @@
-def test(directory):
-    pass
+import os
+# функции для автотестов как на степике
+
+def take_all_files(directory='D:/programm/pycharm/My_studying/files_to_test'):
+    files = os.listdir(directory)
+    return files
+
+
+def take_input_output_values(all_files, directory='D:/programm/pycharm/My_studying/files_to_test'):
+    input_values = []
+    output_values = []
+    for i in all_files:
+        if i.isdigit():
+            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                input_values.append(file.read())
+        elif i.endswith('.clue'):
+            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                output_values.append(list(map(lambda x: x.strip(), file.read().split('\n'))))
+
+    return input_values, output_values
+
