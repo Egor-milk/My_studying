59c3d3787c8430799e23e7ea101e5435daa76fb6
egor <mrpixelegor@mail.ru>
2025-04-18 18:40:29 +0700

    commit

--- 2. Повторение основных конструкций/Part 2/Task 6.py
+++ 2. Повторение основных конструкций/Part 2/Task 6.py
@@ -4,19 +4,25 @@ from autotest import take_input_output_values, take_all_files
 
 def test(input_values, output_values):
     for inp, out in zip(input_values, output_values):
-        print(multilanguage_film(inp))
+        print(same_word(inp))
         print(*out)
         print()
 
-def multilanguage_film(data):
-    data = [i.split(', ') for i in data[1:]]
-    language = set(data[0])
-    for i in data[1:]:
-        language.intersection_update(set(i))
-    if len(language) > 0:
-        return ', '.join(sorted(language))
-    else:
-        return 'Сериал снять не удастся'
+def same_word(data):
+    def isvowel(value):
+        to_return = []
+        vowels = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+        for index, item in enumerate(value):
+            if item in vowels:
+                to_return.append(index)
+        return to_return
+
+    answer = []
+    main_word = isvowel(data[0])
+    for i in data[2:]:
+        if isvowel(i) == main_word:
+            answer.append(i)
+    return ' '.join(answer)
 
 input_values, output_values = take_input_output_values(take_all_files())
 #вывод содержимого файлов
