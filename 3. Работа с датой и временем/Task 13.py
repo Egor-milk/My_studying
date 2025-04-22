8876dd74d238484779c9eb63d1115e0e349035d1
egor <mrpixelegor@mail.ru>
2025-04-22 16:33:28 +0700

    commit

--- 3. Работа с датой и временем/Task 13.py
+++ 3. Работа с датой и временем/Task 13.py
@@ -4,14 +4,16 @@ from autotest import take_input_output_values, take_all_files
 
 def test(input_values, output_values):
     for inp, out in zip(input_values, output_values):
-        print(sort_by_date(inp))
-        print('\n'.join(out))
+        print(sort_date(inp))
+        print(out)
         print()
 
 
-def sort_by_date(lst):
-    lst = map(lambda x: date.fromisoformat(x), lst)
-    return min(lst).strftime('%d-%m (%Y)')
+def sort_date(lst):
+    lst = lst[1:]
+    lst = sorted(map(lambda x: date.fromisoformat(x), lst))
+    lst = map(lambda x: x.strftime('%d/%m/%Y'), lst)
+    return list(lst)
 
 input_values, output_values = take_input_output_values(take_all_files())
 #вывод содержимого файлов
