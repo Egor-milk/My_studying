38c011f05e5f010501c8b92b1d23b10623cd6572
Egor-milk <mrpixelegor@gmail.com>
2025-04-25 15:11:06 +0700

    commit

--- 3. Работа с датой и временем/Task 22.py
+++ 3. Работа с датой и временем/Task 22.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
 with open('D:/programm/PythonProjects/autotest/diary.txt', 'rt', encoding='utf-8') as file:
-    a = file.read().split('\n\n')
-    print(*a)
+    a = file.read().split('\n')
+    a.append('')
+    note = {}
+    temp = []
+    for i in a:
+        if i != '':
+            temp.append(i)
+        else:
+            note[datetime.strptime(temp[0], '%d.%m.%Y; %H:%M')] = temp[1:]
+            temp = []
+    note = dict(sorted(note.items()))
+    for key, value in note.items():
+        print(datetime.strftime(key, '%d.%m.%Y; %H:%M'))
+        print(*value, sep='\n')
+        print()
