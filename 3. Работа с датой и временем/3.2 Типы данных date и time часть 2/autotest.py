c878e7b65f581e9fe13a81b8a3259e18d5bbf956
Egor-milk <mrpixelegor@gmail.com>
2025-04-27 23:45:48 +0700

    commit

---
+++ 3. Работа с датой и временем/3.2 Типы данных date и time часть 2/autotest.py
@@ -0,0 +1,21 @@
+import os
+# функции для автотестов как на степике
+
+def take_all_files(directory='D:/programm/PythonProjects/autotest'):
+    files = os.listdir(directory)
+    return files
+
+
+def take_input_output_values(all_files, directory='D:/programm/PythonProjects/autotest'):
+    input_values = []
+    output_values = []
+    for i in all_files:
+        if i.isdigit():
+            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                input_values.append(file.read())
+        elif i.endswith('.clue'):
+            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                output_values.append(list(map(lambda x: x.strip(), file.read().split('\n'))))
+
+    return input_values, output_values
+
