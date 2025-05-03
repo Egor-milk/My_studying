4306eddbcf0b2a0a3c1de3f010a4bb57119a7f31
Egor <mrpixelegor@mail.ru>
2025-05-03 19:48:20 +0700

    commit

--- 3. Работа с датой и временем/3.7 Calendar/Task 3.py
+++ 3. Работа с датой и временем/3.7 Calendar/Task 3.py
@@ -1,17 +1,20 @@
+from time import strftime
+
 from autotest import take_all_files, take_input_output_values
 import calendar
+from datetime import datetime
 
 
 def main(inp_values, out_values):
     for inp_value, out_value in zip(inp_values, out_values):
-        print(month_calendar(inp_value))
-        print(out_value)
+        print(eng_weekday(inp_value[0]))
+        print(out_value[0])
         print()
 
 
-def month_calendar(lst):
-    cal = calendar.month(int(lst[0]), list(calendar.month_abbr).index(lst[1]))
-    return cal
+def eng_weekday(our_date):
+    our_date = datetime.strptime(our_date, '%Y-%m-%d')
+    return calendar.day_name[our_date.weekday()]
 
 inp, out = (take_input_output_values(take_all_files()))
 main(inp, out)
