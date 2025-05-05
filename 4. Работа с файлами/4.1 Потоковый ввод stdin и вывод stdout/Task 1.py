28d8f32f9d5b6ab82a6120e0af7d2b74f0d38a2e
Egor-milk <mrpixelegor@gmail.com>
2025-05-05 19:44:56 +0700

    commit

--- 4. Работа с файлами/4.1 Потоковый ввод stdin и вывод stdout/Task 1.py
+++ 4. Работа с файлами/4.1 Потоковый ввод stdin и вывод stdout/Task 1.py
@@ -1,3 +1,15 @@
+from autotest import take_all_files, take_input_output_values
 import sys
-answer = [line.strip()[::-1] for line in sys.stdin]
-print(answer)
+
+
+def main(inp_values, out_values):
+    for inp_value, out_value in zip(inp_values, out_values):
+        sys.stdin = inp_value
+        print([line[::-1] for line in sys.stdin.splitlines()])
+        print(out_value)
+        print()
+
+
+
+inp, out = (take_input_output_values(take_all_files()))
+main(inp, out)
---
+++ 4. Работа с файлами/4.1 Потоковый ввод stdin и вывод stdout/autotest.py
@@ -0,0 +1,24 @@
+import os
+# функции для автотестов как на степике
+
+basedir = os.path.abspath(os.getcwd())
+basedir = basedir[:basedir.find('My_studying') + 11]
+
+
+def take_all_files(directory=basedir + '\\autotest'):
+    files = os.listdir(directory)
+    return files
+
+
+def take_input_output_values(all_files, directory=basedir + '\\autotest'):
+    input_values = []
+    output_values = []
+    for i in all_files:
+        if i.isdigit():
+            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                input_values.append(file.read())
+        elif i.endswith('.clue'):
+            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                output_values.append(list(map(lambda x: x.strip(), file.read().split('\n'))))
+    return input_values, output_values
+
