ee6624ee0f78d3eaa6a7b00d915445ece3100ae8
Egor <mrpixelegor@mail.ru>
2025-05-06 19:11:17 +0700

    commit

--- 4. Работа с файлами/4.1 Потоковый ввод stdin и вывод stdout/Task 7.py
+++ 4. Работа с файлами/4.1 Потоковый ввод stdin и вывод stdout/Task 7.py
@@ -1,3 +1,24 @@
+from autotest import take_all_files, take_input_output_values
 import sys
-a = sys.stdin.read()
-sys.stdout.write(a)
+from datetime import datetime
+
+def main(inp_values, out_values):
+    for inp_value, out_value in zip(inp_values, out_values):
+        sys.stdin = inp_value
+        print(filtr(sys.stdin), sep='\n')
+        print(*out_value, sep='\n')
+        print('\n')
+
+def filtr(stding):
+    original_order = [datetime.strptime(i, '%d.%m.%Y') for i in stding.split()]
+    if len(original_order) == len(set(original_order)): #Проверка на повторения
+        if original_order == sorted(original_order):
+            return 'ASC'
+        elif original_order == sorted(original_order, reverse=True):
+            return 'DESC'
+    return 'MIX'
+
+
+inp, out = (take_input_output_values(take_all_files()))
+main(inp, out)
+#print(game(sys.stdin.read()))
