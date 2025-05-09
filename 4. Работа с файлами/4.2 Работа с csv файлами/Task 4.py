bed0898e4748627e934391e25f052279a55c805d
Egor-milk <mrpixelegor@gmail.com>
2025-05-09 13:24:31 +0700

    commit

---
+++ 4. Работа с файлами/4.2 Работа с csv файлами/Task 4.py
@@ -0,0 +1,18 @@
+from autotest import take_all_files, take_input_output_values
+import csv
+
+def main(inp_values, out_values):
+    for inp_value, out_value in zip([inp_values], out_values):
+        print(csv_columns(inp_value))
+        print(*out_value, sep='\n')
+        print()
+
+def csv_columns(d_reader):
+    new_dict = {i:[] for i in d_reader.keys()}
+    for key, value in d_reader.items():
+        new_dict[key].append(value)
+    return new_dict
+
+inp, out = (take_input_output_values(take_all_files(), csv_in=True))
+main(inp, out)
+#print(game(sys.stdin.read()))
