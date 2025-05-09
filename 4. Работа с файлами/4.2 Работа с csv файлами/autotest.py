204f2320794dad46914b8c2b07ffaafa7f766a91
egor <mrpixelegor@mail.ru>
2025-05-09 13:24:31 +0700

    commit

--- 4. Работа с файлами/4.2 Работа с csv файлами/autotest.py
+++ 4. Работа с файлами/4.2 Работа с csv файлами/autotest.py
@@ -1,4 +1,5 @@
 import os
+import csv
 # функции для автотестов как на степике
 
 basedir = os.path.abspath(os.getcwd())
@@ -10,15 +11,26 @@ def take_all_files(directory=basedir):
     return files
 
 
-def take_input_output_values(all_files, directory=basedir):
-    input_values = []
-    output_values = []
-    for i in all_files:
-        if i.isdigit() or i.endswith('.csv'):
-            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
-                input_values.append(file.read())
-        elif i.endswith('.clue'):
-            with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
-                output_values.append(list(map(lambda x: x, file.read().split('\n'))))
-    return input_values, output_values
+def take_input_output_values(all_files, directory=basedir, csv_in=False, csv_delimiter=';', csv_quotechar='"'):
+    if csv_in:
+        output_values = []
+        for i in all_files:
+            if i.isdigit():
+                with open(directory + '\\' + i, 'r', encoding='utf-8') as file:
+                    d_reader = csv.DictReader(file, delimiter=csv_delimiter, quotechar=csv_quotechar)
+            elif i.endswith('.clue'):
+                with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                    output_values.append(list(map(lambda x: x, file.read().split('\n'))))
+            return d_reader, output_values
+    else:
+        input_values = []
+        output_values = []
+        for i in all_files:
+            if i.isdigit():
+                with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                    input_values.append(file.read())
+            elif i.endswith('.clue'):
+                with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                    output_values.append(list(map(lambda x: x, file.read().split('\n'))))
+        return input_values, output_values
 
