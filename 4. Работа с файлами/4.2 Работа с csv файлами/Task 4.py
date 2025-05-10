5db16671dfecde764df49733524663903077b50d
Egor <mrpixelegor@mail.ru>
2025-05-10 15:50:12 +0700

    commit

--- 4. Работа с файлами/4.2 Работа с csv файлами/Task 4.py
+++ 4. Работа с файлами/4.2 Работа с csv файлами/Task 4.py
@@ -2,15 +2,16 @@ from autotest import take_all_files, take_input_output_values
 import csv
 
 def main(inp_values, out_values):
-    for inp_value, out_value in zip([inp_values], out_values):
+    for inp_value, out_value in zip (inp_values, out_values):
         print(csv_columns(inp_value))
-        print(*out_value, sep='\n')
+        print(out_value, sep='\n')
         print()
 
 def csv_columns(d_reader):
-    new_dict = {i:[] for i in d_reader.keys()}
-    for key, value in d_reader.items():
-        new_dict[key].append(value)
+    new_dict = {}
+    for row in d_reader:
+        for key, value in row.items():
+            new_dict[key] = new_dict.get(key, []) + [value]
     return new_dict
 
 inp, out = (take_input_output_values(take_all_files(), csv_in=True))
--- 4. Работа с файлами/4.2 Работа с csv файлами/autotest.py
+++ 4. Работа с файлами/4.2 Работа с csv файлами/autotest.py
@@ -11,20 +11,19 @@ def take_all_files(directory=basedir):
     return files
 
 
-def take_input_output_values(all_files, directory=basedir, csv_in=False, csv_delimiter=';', csv_quotechar='"'):
+def take_input_output_values(all_files, directory=basedir, csv_in=False, csv_delimiter=',', csv_quotechar='"'):
+    input_values = []
+    output_values = []
     if csv_in:
-        output_values = []
         for i in all_files:
             if i.isdigit():
-                with open(directory + '\\' + i, 'r', encoding='utf-8') as file:
-                    d_reader = csv.DictReader(file, delimiter=csv_delimiter, quotechar=csv_quotechar)
+                with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
+                    input_values.append(list((csv.DictReader(file)))) # csv.DictReader работает только с открытым файлом,
+                    # потому преобразовываю объект в список
             elif i.endswith('.clue'):
                 with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
                     output_values.append(list(map(lambda x: x, file.read().split('\n'))))
-            return d_reader, output_values
     else:
-        input_values = []
-        output_values = []
         for i in all_files:
             if i.isdigit():
                 with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
@@ -32,5 +31,5 @@ def take_input_output_values(all_files, directory=basedir, csv_in=False, csv_del
             elif i.endswith('.clue'):
                 with open(directory + '\\' + i, 'rt', encoding='utf-8') as file:
                     output_values.append(list(map(lambda x: x, file.read().split('\n'))))
-        return input_values, output_values
+    return input_values, output_values
 
