0983f28bc82f0f41fad85287dfdd5bc9362f41c2
Egor <mrpixelegor@mail.ru>
2025-05-11 15:19:29 +0700

    asd

--- 4. Работа с файлами/4.2 Работа с csv файлами/Task 8.py
+++ 4. Работа с файлами/4.2 Работа с csv файлами/Task 8.py
@@ -3,17 +3,18 @@ import csv
 from datetime import datetime, date
 
 newest_names = {}
-with open(basedir + '\\name_log.csv', 'rt', encoding='utf-8') as file:
-    d_reader = csv.DictReader(file)
+with open(basedir + '\\name_log.csv', 'rt', encoding='utf-8') as file: #D:\programm\pycharm\My_studying\autotest
+    d_reader = csv.DictReader(file, delimiter=',')
     for row in d_reader:
-        mail = row['email']
-        date1 = datetime.strptime(row['dtime'], '%d/%m/%Y %H:%M')
-        if mail in newest_names:
-            date2 = datetime.strptime(newest_names[mail], '%d/%m/%Y %H:%M')
-        else:
-            date2 = datetime(1, 1, 1)
-        if date1 > date2:
-            newest_names[mail] = [row['username'], row['dtime']]
+        pass
+        #mail = row['email']
+        #date1 = datetime.strptime(row['dtime'], '%d/%m/%Y %H:%M')
+        #if mail in newest_names:
+        #    date2 = datetime.strptime(newest_names[mail], '%d/%m/%Y %H:%M')
+        #else:
+        #    date2 = datetime(1, 1, 1)
+        #if date1 > date2:
+        #    newest_names[mail] = [row['username'], row['dtime']]
 
 with open(basedir + '\\new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
     d_writer = csv.writer(file)
