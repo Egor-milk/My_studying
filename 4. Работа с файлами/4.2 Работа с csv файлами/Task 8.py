e634ecead80cb2b9f6b88d4b5d74bca651e291fc
Egor-milk <mrpixelegor@gmail.com>
2025-05-11 11:46:41 +0700

    commit

--- 4. Работа с файлами/4.2 Работа с csv файлами/Task 8.py
+++ 4. Работа с файлами/4.2 Работа с csv файлами/Task 8.py
@@ -5,4 +5,12 @@ newest_names = {}
 with open(basedir + '\\name_log.csv', 'rt', encoding='utf-8') as file:
      d_reader = csv.DictReader(file)
      for row in d_reader:
-         newest_names[row['email']] = newest_names.get(row['email'], 0) + 1
+         mail = row['email']
+         newest_names[mail] = [row['username'], row['dtime']]
+
+with open(basedir + '\\new_name_log.csv', 'w', encoding='utf-8', newline='') as file:
+    d_writer = csv.writer(file)
+    d_writer.writerow(['username','email', 'dtime'])
+    for key, value in sorted(newest_names.items(), key = lambda x: x[1]):# разобраться как сортировать
+        d_writer.writerow([key, value[0], value[1]])
+        d_writer.writerow([value[0], key, value[1]])
