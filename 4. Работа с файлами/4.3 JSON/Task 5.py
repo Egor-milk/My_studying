4dfb65e0eac69b4dfa88fe1e4b74e4f6e326294c
Egor-milk <mrpixelegor@gmail.com>
2025-05-18 08:45:34 +0700

    a

--- 4. Работа с файлами/4.3 JSON/Task 5.py
+++ 4. Работа с файлами/4.3 JSON/Task 5.py
@@ -1,22 +1,25 @@
 from autotest import take_all_files, take_input_output_values
 import json
 from datetime import datetime
+import sys
 
 def main(inp_values, out_values):
     for inp_value, out_value in zip(inp_values, out_values):
-        exec(inp_value)
-        print(*out_value, sep='\n')
+        print(*return_json_string(inp_value), sep='\n')
         print()
+        print(*out_value, sep='\n')
+        print('\n')
 
-def is_correct_json(json_str):
-    try:
-        json.loads(json_str)
-        return True
-    except json.decoder.JSONDecodeError:
-        return False
-
+def return_json_string(json_str):
+    answer = []
+    for key, value in json.loads(json_str).items():
+        if type(value) == list:
+            value = ', '.join([str(i) for i in value])
+        answer.append(f'{key}: {value}')
+    return answer
 
 
 inp, out = (take_input_output_values(take_all_files()))
+#print(return_json_string(sys.stdin))
 main(inp, out)
 #print(game(sys.stdin.read()))
