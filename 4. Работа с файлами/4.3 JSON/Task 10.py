6b4453a76482deb3d75491a5c532cef9ee2dcd42
Egor-milk <mrpixelegor@gmail.com>
2025-05-30 00:32:45 +0700

    )

--- 4. Работа с файлами/4.3 JSON/Task 10.py
+++ 4. Работа с файлами/4.3 JSON/Task 10.py
@@ -1,11 +1,13 @@
 import json, csv
 from autotest import basedir
+from collections import defaultdict #defaultdict имба оказывается, буду использовать
 
-addresses = {}
+addresses = defaultdict(lambda: defaultdict(list))
 with open(basedir + '\\playgrounds.csv', 'r', encoding='utf-8') as file_to_read:
     d_reader = list(csv.DictReader(file_to_read, delimiter=';'))
+    for item in d_reader:
+        addresses[item['AdmArea']][item['District']].append(item['Address'])
     with open(basedir + '\\addresses.json', 'w', encoding='utf-8') as file_to_write:
-        for i in d_reader:
-            addresses[i['AdmArea']] = addresses.get(i['AdmArea'], {})
-            #addresses[i['AdmArea']][] НЕ ДОДЕЛАНО
+        json.dump(addresses, file_to_write, ensure_ascii=False, indent=4)
+
 print(addresses)
