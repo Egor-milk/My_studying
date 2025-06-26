3b6d280795eafb4b4babd1307588d143680995e8
Egor-milk <mrpixelegor@gmail.com>
2025-06-26 23:55:05 +0700

    )

--- 4. Работа с файлами/4.3 JSON/Task 13.py
+++ 4. Работа с файлами/4.3 JSON/Task 13.py
@@ -1,10 +1,18 @@
 from autotest import basedir
 import json, csv
 from collections import defaultdict
+from datetime import datetime
 
-best_scores = {}
+all_scores = defaultdict(list) #при обращении к best_scores[key1] создает список
 with open(basedir + '\\exam_results.csv', 'r', encoding='utf-8') as file_to_read:
     d_reader = csv.DictReader(file_to_read, delimiter=',')
     for line in d_reader:
         full_name = line['name'] + ' ' + line['surname']
-        best_scores[full_name] = best_scores.get(full_name, {}) #ЕЩЕ НЕ РАБОТАЕТ
+        all_scores[full_name].append(line)
+best_value = []
+for value in all_scores.values():
+    best_value.append(max(sorted(value, key= lambda y: datetime.strptime(y['date_and_time'], '%Y-%m-%d %H:%M:%S'),
+                                 reverse= True), key=lambda x: x['score']))
+best_value.sort(key=lambda x: x['email'])
+with open(basedir + '\\best_scores.json', 'w', encoding='utf-8') as file_to_write:
+    json.dump(best_value, file_to_write, ensure_ascii=False, indent=4)
