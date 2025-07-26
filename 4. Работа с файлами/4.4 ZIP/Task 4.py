2dde138215ab07669aaa8792e6e2f9f09588e090
Egor <mrpixelegor@mail.ru>
2025-07-26 19:27:03 +0700

    )

--- 4. Работа с файлами/4.4 ZIP/Task 4.py
+++ 4. Работа с файлами/4.4 ZIP/Task 4.py
@@ -2,9 +2,13 @@ from zipfile import ZipFile
 from autotest import basedir
 from datetime import datetime, timedelta
 
-
-with ZipFile(basedir / 'workbook.zip', 'r') as zip:
+filtered_files = []
+with ZipFile(basedir + '\\' +  'workbook.zip', 'r') as zip:
     files = zip.infolist()
     for file in files:
         if not file.is_dir():#не доделано
-            print(file.date_time)
+            if datetime.strptime('.'.join(map( lambda x:str(x), file.date_time)), '%Y.%m.%d.%H.%M.%S') > \
+                    datetime(2021, 11, 30, 14, 22):
+                filtered_files.append(file.filename[file.filename.rfind('/') + 1:])
+filtered_files.sort()
+print(*filtered_files, sep='\n')
