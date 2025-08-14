e70b33d54ac0a8d54956d76e18354abc01d12174
Egor <mrpixelegor@mail.ru>
2025-08-15 00:11:21 +0700

    )

--- 4. Работа с файлами/4.4 ZIP/Task 5.py
+++ 4. Работа с файлами/4.4 ZIP/Task 5.py
@@ -7,11 +7,13 @@ with ZipFile(basedir + '\\' + 'workbook.zip', 'r') as zipFile:
     infolist = zipFile.infolist()
     for info in infolist:
         if not info.is_dir():
-            zip_info.append([info.filename[info.filename.rfind('/') + 1:], info.date_time, info.file_size, info.compress_size])
+            zip_info.append([info.filename[info.filename.rfind('/') + 1:], [str(i).rjust(2, '0')
+                                                                            for i in info.date_time],
+                             info.file_size, info.compress_size])
 
 for i in sorted(zip_info, key=lambda x:x[0]):
     print(f'''  {i[0]}
-        Дата модификации файла: {i[1]}
+        Дата модификации файла: {i[1][0]}-{i[1][1]}-{i[1][2]} {i[1][3]}:{i[1][4]}:{i[1][5]}
         Объем исходного файла: {i[2]} байт(а)
         Объем сжатого файла: {i[3]} байт(а)
     ''')
