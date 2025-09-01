ffaf9fa1eb1d1580bc7f53df119f8ed36cd7bfeb
Egor-milk <mrpixelegor@gmail.com>
2025-09-02 00:36:25 +0700

    )

--- 4. Работа с файлами/4.4 ZIP/Task 8.py
+++ 4. Работа с файлами/4.4 ZIP/Task 8.py
@@ -1,1 +1,13 @@
 from autotest import basedir
+from zipfile import ZipFile
+
+
+def extract_this(zip_name, *args):
+    with ZipFile(basedir + '\\' + zip_name, 'r') as myzip:
+        if len(args) > 0:
+            for i in args:
+                myzip.extract(i, basedir)
+        else:
+            myzip.extractall(basedir)
+
+extract_this('test.zip', 'test/Картинки/1.jpg')
