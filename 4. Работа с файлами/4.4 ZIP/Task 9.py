58964aa1cd83182af8a40f67ab00d9f1708669c2
Egor <mrpixelegor@mail.ru>
2025-09-02 21:42:45 +0700

    )

--- 4. Работа с файлами/4.4 ZIP/Task 9.py
+++ 4. Работа с файлами/4.4 ZIP/Task 9.py
@@ -2,6 +2,12 @@ from autotest import basedir
 from zipfile import ZipFile
 import json
 
+def is_json(file):
+    try:
+        json_object = json.loads(file)
+    except ValueError:
+        return False
+    return True
 
 with ZipFile(basedir + '\\' + 'data.zip', 'r') as myzip:
     pass
