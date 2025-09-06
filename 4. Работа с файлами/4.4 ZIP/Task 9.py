863b0ceb6126ae1d32ad6ea52e5f642e33462de1
Egor-milk <mrpixelegor@gmail.com>
2025-09-06 19:47:32 +0700

    )

--- 4. Работа с файлами/4.4 ZIP/Task 9.py
+++ 4. Работа с файлами/4.4 ZIP/Task 9.py
@@ -2,8 +2,35 @@ import shutil
 
 from autotest import basedir
 from zipfile import ZipFile
-import json
-import os
+import json, os, sys
+
+path_to_zip = basedir + '\\' + 'data.zip'
+path_to_unzip = basedir + '\\' + 'temp'
+
+def extract_flattened():
+    """
+    Извлекает все файлы из архива в одну директорию
+    """
+    # Создаем целевую директорию
+    os.makedirs(path_to_unzip, exist_ok=True)
+
+    with ZipFile(path_to_zip, 'r') as zip_file:
+        for zip_info in zip_file.infolist():
+            # Пропускаем директории
+            if zip_info.is_dir() or not zip_info.filename.endswith('.json'):
+                continue
+
+            # Берем только имя файла (игнорируем путь)
+            filename = os.path.basename(zip_info.filename)
+
+            # Устанавливаем новое имя (только файл)
+            zip_info.filename = filename
+
+            # Извлекаем
+            zip_file.extract(zip_info, path_to_unzip)
+            print(f"Извлечен: {filename}")
+
+
 
 def is_json(file):
     try:
@@ -13,13 +40,11 @@ def is_json(file):
         return False
     return True
 
-with ZipFile(basedir + '\\' + 'data.zip', 'r') as myzip: #НИХУЯ НЕ РАБОТАЕТ БЛЯТЬ
-    path_to_temp = (basedir + '\\' + 'temp').replace('/', '\\')
-    for i in myzip.namelist():
-        myzip.extract(i, path_to_temp)
-        path_to_file = path_to_temp + '\\' + i
-        if os.path.isfile(path_to_file) and is_json(path_to_file):
-            os.remove(path_to_file)
+for i in os.listdir(path_to_unzip):
+    if not is_json(i):
+        os.remove(os.path.join(path_to_unzip, i))
+
+extract_flattened()
 
 #print(is_json('D://programm//pyc
 #harm//My_studying//autotest//player2.json'))
