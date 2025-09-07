276e0650c7646bc296ffd2a1a0d06490bae2da1a
Egor <mrpixelegor@mail.ru>
2025-09-07 16:11:56 +0700

    )

--- 4. Работа с файлами/4.4 ZIP/Task 9.py
+++ 4. Работа с файлами/4.4 ZIP/Task 9.py
@@ -2,56 +2,27 @@ import shutil
 
 from autotest import basedir
 from zipfile import ZipFile
-import json, os, sys
+import json
 
 path_to_zip = basedir + '\\' + 'data.zip'
-path_to_unzip = basedir + '\\' + 'temp'
-
-def extract_flattened():
-    """
-    Извлекает все файлы из архива в одну директорию
-    """
-    # Создаем целевую директорию
-    os.makedirs(path_to_unzip, exist_ok=True)
-
-    with ZipFile(path_to_zip, 'r') as zip_file:
-        for zip_info in zip_file.infolist():
-            # Пропускаем директории и файлы не json
-            if zip_info.is_dir() or not zip_info.filename.endswith('.json'):
-                continue
-
-            # Берем только имя файла (игнорируем путь)
-            filename = os.path.basename(zip_info.filename)
-
-            # Устанавливаем новое имя (только файл)
-            zip_info.filename = filename
-
-            # Извлекаем
-            zip_file.extract(zip_info, path_to_unzip) #Окзаывется экстракт можно использовать и так, с объектом infolist
-            print(f"Извлечен: {filename}")
-
-def delete_invalid_json(path):
-
-
-    for i in os.listdir(path):
-        if not is_json(i):
-            os.remove(os.path.join(path, i))
-
 
 def is_json(file):
     try:
-        json_object = json.loads(file)
+        json_object = json.loads(file.read().decode('utf-8'))
     except:
-        return False
-    return True
+        return False, []
+    return True, json_object
 
-answer = []
+arsenal_football_player = set()
+valid_json = []
 with ZipFile(path_to_zip, 'r') as zip_file:
     for file_name in zip_file.namelist():
         if file_name.endswith('.json'):
             with zip_file.open(file_name) as f:
-                if is_json(file_name):
-                    answer.append(file_name)
+                is_js, json_object = is_json(f)
+                if is_js:
+                    if json_object['team'] == 'Arsenal':
+                        arsenal_football_player.add(f'{json_object['first_name']} {json_object['last_name']}')
+print(*sorted(arsenal_football_player), sep='\n')
 
-print(answer)
 
