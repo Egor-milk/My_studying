118ffae26487145d61bb17959bd9793038d87bae
egor <mrpixelegor@mail.ru>
2025-09-24 10:53:09 +0700

    commit

--- 6. Дополнительные типы коллекций/6.6 OrderedDict/Task 1.py
+++ 6. Дополнительные типы коллекций/6.6 OrderedDict/Task 1.py
@@ -1,6 +1,20 @@
+#Вам доступен словарь data. Дополните приведенный ниже код, чтобы он вывел данный словарь,
+# расположив его элементы в обратном порядке.
+
+#Примечание. Например, если бы словарь data имел вид:
+
+#data = OrderedDict(key1='value1', key2='value2', key3='value3')
+#то программа должна была бы вывести:
+
+#OrderedDict([('key3', 'value3'), ('key2', 'value2'), ('key1', 'value1')])
+
+
 from collections import OrderedDict
-cloth = OrderedDict({'name': 'pants', 'size': 'm', 'color': 'grey'})
-cloth['name'] = 'shirt'
-cloth.update(size='s')
-for key, value in cloth.items():
-        print(f'{key}: {value}')
+
+data = OrderedDict({'Name': 'Брусника', 'IsNetObject': 'да', 'OperatingCompany': 'Брусника', 'TypeObject': 'кафе',
+                    'AdmArea': 'Центральный административный округ', 'District': 'район Арбат',
+                    'Address': 'город Москва, переулок Сивцев Вражек, дом 6/2', 'SeatsCount': '10'})
+
+data1 = reversed(data.items())
+print(OrderedDict(data1))
+
