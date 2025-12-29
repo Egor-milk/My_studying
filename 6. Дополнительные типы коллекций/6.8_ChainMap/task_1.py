5f6ea9e1b66b02b0394b1ffc802694a32ee0e73b
Egor <mrpixelegor@mail.ru>
2025-12-29 13:41:15 +0700

    )

--- 6. Дополнительные типы коллекций/6.8_ChainMap/task_1.py
+++ 6. Дополнительные типы коллекций/6.8_ChainMap/task_1.py
@@ -1,7 +1,12 @@
 from collections import ChainMap
 import json
 
+
+summary = 0
 with open('zoo.json', 'r', encoding='utf-8') as f:
     file = json.load(f)
-    a = ChainMap(file)
+    a = ChainMap(*file)
 print(a)
+for k in a:
+    summary += a[k]
+print(summary)
